c789485597a06e3a68f183a9b1e0d3982efec723
MertcanYavasoglu <mertcanyavasoglu10@gmail.com>
2023-09-24 21:14:56 +0300

    update

--- Snake Game/main.py
+++ Snake Game/main.py
@@ -13,7 +13,10 @@ canvas.configure(bg='black')
 canvas.pack()
 
 direction = 3  # 0-up  1-down  2-left  3-right
-player = canvas.create_rectangle(0, 0, 30, 30, fill='green')
+player = canvas.create_rectangle(600, 600, 630, 630, fill='green')
+berry_x = random.randrange(0, 1890, 30)
+berry_y = random.randrange(0,1050, 30)
+berry = canvas.create_rectangle(berry_x, berry_y, berry_x+30, berry_y+30, fill='white')
 
 def move_up(event):
     global direction
@@ -43,11 +46,31 @@ def check_coords():
     return "no_collision"
 
 def berry_spawn():
-    berry_x = random.randrange(0, 1890, 30)
-    berry_y = random.randrange(0,1050, 30)
-    berry = canvas.create_rectangle(berry_x, berry_y, berry_x+30, berry_y+30, fill='white')
+    global berry
+    global berry_x
+    global berry_y
     coords_berry = canvas.coords(berry)
     coords_player = canvas.coords(player)
+    base_x = 0
+    base_y= 0
+    moveable_limit_x_left = canvas.coords(berry)[0]
+    moveable_limit_x_right = 1920 - canvas.coords(berry)[2]
+    moveable_limit_y_down = 1080 - canvas.coords(berry)[3]
+    moveable_limit_y_up = canvas.coords(berry)[1]
+    if coords_berry == coords_player:
+        list_x = moveable_limit_x_left, moveable_limit_x_right
+        list_y = moveable_limit_y_down, moveable_limit_y_up
+        choice_x = random.choice(list_x)
+        choice_y = random.choice(list_y)
+        if choice_x == moveable_limit_x_right:
+            base_x = coords_berry[2]
+        if choice_y == moveable_limit_y_down:
+            base_y = coords_berry[3]
+
+        berry_move_x = random.randrange(base_x, choice_x, 30)
+        berry_move_y = random.randrange(base_y, choice_y, 30)
+        canvas.move(berry, berry_move_x, berry_move_y)
+    window.after(250, berry_spawn)
 
 def game_loop():
     collision_result = check_coords()
